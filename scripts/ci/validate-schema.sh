6a65f9d9e1af4de17a762e4ed6c662cb51420b1e
Kevin Wojkovich <kevin.wojkovich@sourcegraph.com>
2022-01-03 13:35:20 -0600

    Debug

--- scripts/ci/validate-schema.sh
+++ scripts/ci/validate-schema.sh
@@ -5,8 +5,13 @@ set -euf -o pipefail
 ### Install kubeval
 wget https://github.com/yannh/kubeconform/releases/download/v0.4.12/kubeconform-linux-amd64.tar.gz
 tar xf kubeconform-linux-amd64.tar.gz 
+chmod +x kubeconform
 sudo cp kubeconform /usr/local/bin
 
-### Run kubeval, validate k8s schema
+### Run kubeconform, validate k8s schema
+echo "Generating template output"
 helm template sourcegraph-helm-default ./sourcegraph >sourcegraph-helm-default.yaml
+
+echo "Verifying schema"
 kubeconform -strict -schema-location https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/ sourcegraph-helm-default.yaml
+
