d1df5be2f9ad487510c40dccb5a70bc0b8927cfb
Kevin Wojkovich <kevin.wojkovich@sourcegraph.com>
2022-01-03 13:28:25 -0600

    Shfmt and add soft_fail for warning on lint

--- scripts/ci/lint.sh
+++ scripts/ci/lint.sh
@@ -3,4 +3,14 @@
 set -euf -o pipefail
 
 ### Run the helm tests
-helm lint sourcegraph
+LINT_OUTPUT=$(helm lint sourcegraph)
+ORG_STATUS=$?
+
+printf "\n\n===== Lint Output =====\n$LINT_OUTPUT\n"
+
+LINT_OUTPUT_LOWER=$(echo "$LINT_OUTPUT" | awk '{print tolower($0)}')
+if grep -q "warning" <<<"$LINT_OUTPUT_LOWER"; then
+	exit 255
+else
+	exit $ORG_STATUS
+fi
--- scripts/ci/validate-schema.sh
+++ scripts/ci/validate-schema.sh
@@ -8,5 +8,5 @@ tar xf kubeval-linux-amd64.tar.gz
 sudo cp kubeval /usr/local/bin
 
 ### Run kubeval, validate k8s schema
-helm template sourcegraph-helm-default ./sourcegraph > sourcegraph-helm-default.yaml
+helm template sourcegraph-helm-default ./sourcegraph >sourcegraph-helm-default.yaml
 kubeval --strict --schema-location https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/ sourcegraph-helm-default.yaml
