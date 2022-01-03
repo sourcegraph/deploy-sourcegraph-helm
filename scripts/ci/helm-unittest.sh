c0b10e023beea22ddd4472271f3910d14677d182
Kevin Wojkovich <kevin.wojkovich@sourcegraph.com>
2022-01-03 11:29:28 -0600

    Add script to run unittests

---
+++ scripts/ci/helm-unittest.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+### Install Helm
+curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
+sudo apt-get install apt-transport-https --yes
+echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm
+
+### Install the helm-unittest plugin
+helm plugin install https://github.com/quintush/helm-unittest
+
+### Run the helm tests
+helm -3 unittest sourcegraph
