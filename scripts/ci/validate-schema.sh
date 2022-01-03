bd207e4b8bd0720b3d3c2eee739aac4bc7416ad7
Kevin Wojkovich <kevin.wojkovich@sourcegraph.com>
2022-01-03 13:40:51 -0600

    Add output back, kubeconform quiet by default

--- scripts/ci/validate-schema.sh
+++ scripts/ci/validate-schema.sh
@@ -4,14 +4,12 @@ set -euf -o pipefail
 
 ### Install kubeval
 wget https://github.com/yannh/kubeconform/releases/download/v0.4.12/kubeconform-linux-amd64.tar.gz
-tar xf kubeconform-linux-amd64.tar.gz 
+tar xf kubeconform-linux-amd64.tar.gz
 chmod +x kubeconform
 sudo cp kubeconform /usr/local/bin
 
 ### Run kubeconform, validate k8s schema
-echo "Generating template output"
+echo "Generating template output..."
 helm template sourcegraph-helm-default ./sourcegraph >sourcegraph-helm-default.yaml
 
-echo "Verifying schema"
-kubeconform -strict -schema-location https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/ sourcegraph-helm-default.yaml
-
+kubeconform -verbose -summary -strict -schema-location https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/ sourcegraph-helm-default.yaml
