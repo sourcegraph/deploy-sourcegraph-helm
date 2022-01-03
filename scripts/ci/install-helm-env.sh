8454da1193c12ceb83eae4715f6a4c0e7caba6b8
Kevin Wojkovich <kevin.wojkovich@sourcegraph.com>
2022-01-03 11:41:21 -0600

    Fixup

--- scripts/ci/install-helm-env.sh
+++ scripts/ci/install-helm-env.sh
@@ -7,9 +7,3 @@ sudo apt-get install apt-transport-https --yes
 echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
 sudo apt-get update
 sudo apt-get install helm
-
-### Install the helm-unittest plugin
-helm plugin install https://github.com/quintush/helm-unittest
-
-### Run the helm tests
-helm unittest -3q sourcegraph
