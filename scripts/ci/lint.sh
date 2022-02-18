13b00ccc17459c92fcb093f742f98a5acba7e863
Michael Lin <mlzc@hey.com>
2022-02-18 14:41:44 -0800

    Add sourcegraph-migrator chart

--- scripts/ci/lint.sh
+++ scripts/ci/lint.sh
@@ -3,14 +3,20 @@
 set -euf -o pipefail
 
 ### Run the helm tests
-LINT_OUTPUT=$(helm lint charts/sourcegraph)
-ORG_STATUS=$?
+function lint_chart() {
+  echo "Linting chart $1"
+  LINT_OUTPUT=$(helm lint charts/$1)
+  ORG_STATUS=$?
 
-printf "\n\n===== Lint Output =====\n$LINT_OUTPUT\n"
+  printf "\n\n===== Lint Output =====\n$LINT_OUTPUT\n"
 
-LINT_OUTPUT_LOWER=$(echo "$LINT_OUTPUT" | awk '{print tolower($0)}')
-if grep -q "warning" <<<"$LINT_OUTPUT_LOWER"; then
-	exit 255
-else
-	exit $ORG_STATUS
-fi
+  LINT_OUTPUT_LOWER=$(echo "$LINT_OUTPUT" | awk '{print tolower($0)}')
+  if grep -q "warning" <<<"$LINT_OUTPUT_LOWER"; then
+    exit 255
+  else
+    exit $ORG_STATUS
+  fi
+}
+
+lint_chart "sourcegraph"
+lint_chart "sourcegraph-migrator"
