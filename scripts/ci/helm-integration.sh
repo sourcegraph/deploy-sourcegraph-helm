803eb7dc00196468d0223f10da7776ba799cc084
Kevin Wojkovich <kevin.wojkovich@sourcegraph.com>
2022-05-03 07:39:14 -0500

    remove plugin install:

--- scripts/ci/helm-integration.sh
+++ scripts/ci/helm-integration.sh
@@ -3,12 +3,8 @@
 set -euf -o pipefail
 
 
-# Install asdf terraform plugin
-#asdf plugin add kind https://github.com/virtualstaticvoid/asdf-kind.git 
-TERRAFORM_CHECK=$(asdf which terraform)
-if [[ $? -ne 0 ]]; then
-  asdf plugin-add terraform https://github.com/asdf-community/asdf-hashicorp.git
-fi
+# Install asdf terraform plugin - managed by stateless agent configuration
+# asdf plugin-add terraform https://github.com/asdf-community/asdf-hashicorp.git
 
 # Install terraform via asdf
 asdf install
