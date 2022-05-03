0c2958c04d126e276e1718445b6bd5e44eabe687
Kevin Wojkovich <kevin.wojkovich@sourcegraph.com>
2022-05-03 07:37:42 -0500

    conditionally add plugin if not installed

--- scripts/ci/helm-integration.sh
+++ scripts/ci/helm-integration.sh
@@ -5,7 +5,10 @@ set -euf -o pipefail
 
 # Install asdf terraform plugin
 #asdf plugin add kind https://github.com/virtualstaticvoid/asdf-kind.git 
-asdf plugin-add terraform https://github.com/asdf-community/asdf-hashicorp.git
+TERRAFORM_CHECK=$(asdf which terraform)
+if [[ $? -ne 0 ]]; then
+  asdf plugin-add terraform https://github.com/asdf-community/asdf-hashicorp.git
+fi
 
 # Install terraform via asdf
 asdf install
