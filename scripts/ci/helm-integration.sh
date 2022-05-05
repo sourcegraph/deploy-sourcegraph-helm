b18a2fc213cb3eb26e6eef3372c8ef0c56af2ca5
Kevin Wojkovich <kevin.wojkovich@sourcegraph.com>
2022-05-05 08:50:49 -0500

    install kubectl

--- scripts/ci/helm-integration.sh
+++ scripts/ci/helm-integration.sh
@@ -5,11 +5,17 @@ set -euf -o pipefail
 
 # Install asdf terraform plugin - managed by stateless agent configuration
 # asdf plugin-add terraform https://github.com/asdf-community/asdf-hashicorp.git
+asdf plugin-add kubectl https://github.com/asdf-community/asdf-kubectl.git
 
 # Install terraform via asdf
 asdf install
+asdf reshim
 
 cd scripts/ci/terraform
 
-asdf exec terraform init
-asdf exec terraform apply -auto-approve
+terraform init
+terraform apply -auto-approve
+
+echo "TESTS would happen here"
+
+terraform destroy -auto-approve
