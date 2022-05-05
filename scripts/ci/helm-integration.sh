8eb9874d87df20d995143ce78e1e0bea59b3a59a
Kevin Wojkovich <kevin.wojkovich@sourcegraph.com>
2022-05-05 08:52:08 -0500

    fixup

--- scripts/ci/helm-integration.sh
+++ scripts/ci/helm-integration.sh
@@ -5,7 +5,7 @@ set -euf -o pipefail
 
 # Install asdf terraform plugin - managed by stateless agent configuration
 # asdf plugin-add terraform https://github.com/asdf-community/asdf-hashicorp.git
-asdf plugin-add kubectl https://github.com/asdf-community/asdf-kubectl.git
+#asdf plugin-add kubectl https://github.com/asdf-community/asdf-kubectl.git
 
 # Install terraform via asdf
 asdf install
