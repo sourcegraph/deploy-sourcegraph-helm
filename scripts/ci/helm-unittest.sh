9b432ab9817e7f46274ec042e32c73650487267e
Michael Lin <mlzc@hey.com>
2025-12-15 23:15:24 -0800

    fix(ci): helm unittest is broken (#787)
    see comments
    
    ### Test plan
    
    CI pass now
    
    (cherry picked from commit b9369b8009ff2a652c3e0a6530316b8591fbd679)

--- scripts/ci/helm-unittest.sh
+++ scripts/ci/helm-unittest.sh
@@ -2,8 +2,12 @@
 
 set -euf -o pipefail
 
+# 1.0.3 is broken now
+# https://github.com/helm-unittest/helm-unittest/issues/790
+HELM_UNITTEST_VERSION="v1.0.2"
+
 ### Install the helm-unittest plugin
-helm plugin install https://github.com/helm-unittest/helm-unittest
+helm plugin install https://github.com/helm-unittest/helm-unittest --version "$HELM_UNITTEST_VERSION"
 
 ### Run the helm tests
 helm unittest -q charts/sourcegraph
